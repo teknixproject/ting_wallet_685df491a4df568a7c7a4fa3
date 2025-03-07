69abddd239eb056f62f1d18cfb58def62e4b3462
Thai Bao <nguyenthaibao.haugiang@gmail.com>
2025-03-07 13:47:58 +0700

    preview-ui config

--- components/grid-systems/ClientWrapGridSystem.tsx
+++ components/grid-systems/ClientWrapGridSystem.tsx
@@ -1,16 +1,26 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import GridSystemContainer from "@/components/grid-systems";
-import { getDeviceType } from "@/lib/utils";
-import { useConstructorDataAPI } from "@/app/actions/use-constructor";
-import LoadingPage from "./loadingPage";
+import LoadingPage from './loadingPage';
+import _ from 'lodash';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+import GridSystemContainer from '@/components/grid-systems';
+import SandPackUI from './preview-ui';
+import { getDeviceType } from '@/lib/utils';
+import { useConstructorDataAPI, usePreviewUI } from '@/app/actions/use-constructor';
 
 export default function ClientWrapper(props: any) {
-  const { layout, isLoading } = useConstructorDataAPI(
-    props.documentId,
-    props.pathName
-  );
+  const isPreviewUI = _.get(props, 'pathName') === 'preview-ui';
+
+  if (isPreviewUI) {
+    return <PreviewUI {...props} />;
+  }
+  return <RenderUIClient {...props} />;
+}
+
+const RenderUIClient = (props: any) => {
+  const { layout, isLoading } = useConstructorDataAPI(props.documentId, props.pathName);
 
   const [deviceType, setDeviceType] = useState(getDeviceType());
   const selectedLayout = layout[deviceType] ?? layout ?? {};
@@ -19,9 +29,9 @@ export default function ClientWrapper(props: any) {
     const handleResize = () => {
       setDeviceType(getDeviceType());
     };
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window.removeEventListener('resize', handleResize);
     };
   }, [props.page]);
 
@@ -37,4 +47,39 @@ export default function ClientWrapper(props: any) {
       deviceType={deviceType}
     />
   );
-}
+};
+
+const PreviewUI = (props: any) => {
+  const searchParams = useSearchParams();
+  const projectId = searchParams.get('projectId');
+
+  const [deviceType, setDeviceType] = useState(getDeviceType());
+  const { dataPreviewUI, isLoading } = usePreviewUI(projectId ?? '');
+  const isComponent = _.get(dataPreviewUI, 'data.typePreview') === 'page';
+  const layout = _.get(dataPreviewUI, 'data.previewData');
+
+  useEffect(() => {
+    const handleResize = () => setDeviceType(getDeviceType());
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+
+  return (
+    <div className="component-preview-container">
+      {isComponent ? (
+        <GridSystemContainer
+          isLoading={isLoading}
+          {...props}
+          page={layout[deviceType] || {}}
+          deviceType={deviceType}
+        />
+      ) : (
+        <SandPackUI dataPreviewUI={dataPreviewUI} />
+      )}
+    </div>
+  );
+};
