90e2aabfbebe1bb814adbdc61710c8d57f5e2988
Thai Bao <nguyenthaibao.haugiang@gmail.com>
2025-03-08 21:33:37 +0700

    custom preview & components

--- components/commons/image.tsx
+++ components/commons/image.tsx
@@ -1,5 +1,5 @@
-import _ from "lodash";
-import { CSSProperties } from "react";
+import _ from 'lodash';
+import { CSSProperties } from 'react';
 
 interface ImageProps {
   data?: any;
@@ -7,37 +7,28 @@ interface ImageProps {
 }
 
 const Image = ({ data, style }: ImageProps) => {
-  const url = _.get(
-    data,
-    "url",
-    "https://www.10wallpaper.com/wallpaper/1680x1050/1609/Apple_iOS_9_iPhone_6s_Plus_HD_Wallpaper_04_1680x1050.jpg"
-  );
+  const url = _.get(data, 'url', '/default-bg.png');
 
   const newStyle: CSSProperties = {
     ...style,
-    position: "initial",
-    transform: "",
+    position: 'initial',
+    transform: '',
     margin: 0,
     padding: 0,
-    maxHeight: "",
-    maxWidth: "",
-    width: "100%",
-    height: "100%",
+    maxHeight: '',
+    maxWidth: '',
+    width: '100%',
+    height: '100%',
   };
 
-  const isBg = _.get(style, "background");
+  const isBg = _.get(style, 'background');
 
   return (
     <div
-      style={{ width: "100%", height: "100%" }}
+      style={{ width: '100%', height: '100%' }}
       className="relative overflow-hidden flex items-center justify-center"
     >
-      <img
-        style={newStyle}
-        src={url}
-        alt="Image"
-        className="w-full h-auto object-cover"
-      />
+      <img style={newStyle} src={url} alt="Image" className="w-full h-auto object-cover" />
       {isBg && (
         <div
           className="absolute inset-0 pointer-events-none"
