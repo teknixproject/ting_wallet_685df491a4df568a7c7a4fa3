d863093e5ef0aceb3ae8b58f0614848770c24c97
khoi_2212 <kkhoi600@gmail.com>
2025-03-24 16:12:37 +0700

    update handle action in button and input

--- components/commons/inputText.tsx
+++ components/commons/inputText.tsx
@@ -3,6 +3,7 @@ import React, { ChangeEvent } from 'react';
 import { CSSProperties } from 'styled-components';
 
 import { stateManagementStore } from '@/stores/stateManagement';
+import { variableUtil } from '@/uitls';
 
 import { GridItem } from '../grid-systems/const';
 
@@ -11,14 +12,23 @@ type Props = { data: GridItem };
 const InputText: React.FC<Props> = ({ data }) => {
   const title = _.get(data, 'dataSlice.title', 'Text');
   const style = _.get(data, 'dataSlice.style', {});
+  const newStyle: CSSProperties = {
+    ...style,
+    position: 'initial',
+    transform: '',
+    margin: 0,
+    padding: 0,
+    maxHeight: '',
+    maxWidth: '',
+    width: '100%',
+    height: '100%',
+  };
   const variableName = _.get(data, 'dataSlice.variableName', '');
   console.log('🚀 ~ variableName:', variableName);
-  const newStyle: CSSProperties = {
-    ...(typeof style === 'object' && style !== null ? style : {}),
-  };
+
   const { findVariable, updateDocumentVariable } = stateManagementStore();
   const variableChange = findVariable({ type: 'componentState', name: variableName });
-
+  const { extractAllValuesFromTemplate } = variableUtil;
   console.log('🚀 ~ variableChange:', variableChange);
   const updateVarialbe = _.debounce((key, value) => {
     updateDocumentVariable({
@@ -28,9 +38,10 @@ const InputText: React.FC<Props> = ({ data }) => {
   }, 300);
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!variableName) return;
+    const key = extractAllValuesFromTemplate(variableName);
     const value = e.target.value;
 
-    updateVarialbe(variableName, value);
+    updateVarialbe(key, value);
   };
 
   return <input type="text" defaultValue={title} style={newStyle} onChange={handleInputChange} />;
--- components/commons/text.tsx
+++ components/commons/text.tsx
@@ -1,21 +1,49 @@
 import _ from 'lodash';
-import { CSSProperties } from 'react';
+import { CSSProperties, useEffect, useState } from 'react';
+
+import { stateManagementStore } from '@/stores/stateManagement';
+import { TTypeSelectState } from '@/types';
+import { variableUtil } from '@/uitls';
+
+import { GridItem } from '../grid-systems/const';
 
 interface TextProps {
-  data?: any;
+  data: GridItem;
   style?: CSSProperties;
 }
 
 const Text = ({ data, style }: TextProps) => {
-  const title = _.get(data, 'title', 'Text');
+  const [title, setTitle] = useState<string>(_.get(data, 'dataSlice.title', 'Text'));
+  const [variableName, setVariableName] = useState<string>(
+    _.get(data, 'dataSlice.variableName', '')
+  );
+  const [typeStore, setTypeStore] = useState<TTypeSelectState>(
+    _.get(data, 'dataSlice.typeStore', 'appState')
+  );
 
+  const { findVariable, appState } = stateManagementStore();
+  console.log('🚀 ~ Text ~ appState:', appState);
+
+  const { extractAllValuesFromTemplate } = variableUtil;
+
+  useEffect(() => {
+    if (variableName && typeStore) {
+      const key = extractAllValuesFromTemplate(variableName);
+      const valueInStore = findVariable({
+        type: typeStore,
+        name: key ?? '',
+      });
+      console.log('🚀 ~ useEffect ~ valueInStore:', valueInStore);
+      setTitle(valueInStore?.value ?? 'Text');
+    }
+  }, [variableName, typeStore, appState]);
   const newStyle: CSSProperties = {
     ...style,
   };
 
   return (
     <div style={newStyle} className="text-[#858585]">
-      {title}
+      {JSON.stringify(title)}
     </div>
   );
 };
