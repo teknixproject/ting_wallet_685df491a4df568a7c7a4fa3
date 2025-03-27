534aa9d8ea8c3e45e15f0fec21ba37889c24c56a
nguyenthaibao.haugiang@gmail.com <170920443+ryannguyen2000@users.noreply.github.com>
2025-03-27 17:18:53 +0700

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  // safelist: [
+  //   "bg-red-600", // Thêm class bạn muốn giữ
+  //   "col-span-1",
+  //   "row-span-1",
+  //   "grid-rows-1",
+  //   "gap-4",
+  //   "justify-start",
+  //   "items-start",
+  //   "flex",
+  //   // Thêm các class khác từ API nếu cần
+  // ],
   theme: {
     extend: {},
   },
