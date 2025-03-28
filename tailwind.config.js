10c5f1b52de6d176bb98efb080bfad76cccc09d4
khoi_2212 <kkhoi600@gmail.com>
2025-03-28 08:24:55 +0700

    add safeList class tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import { tailwindConfigSafeList } from './configTailwind';
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safeList: [...tailwindConfigSafeList],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
