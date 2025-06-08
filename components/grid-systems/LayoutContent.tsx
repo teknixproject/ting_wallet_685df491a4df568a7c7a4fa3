1a5e55b54493e6995a028fa5dc00bc2fe488d0e4
teknixproject <170920443+ryannguyen2000@users.noreply.github.com>
2025-06-08 18:10:41 +0700

    Update LayoutContent.tsx

--- components/grid-systems/LayoutContent.tsx
+++ components/grid-systems/LayoutContent.tsx
@@ -1,14 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // components/LayoutContent.tsx
 'use client';
 import { useState, useEffect, useMemo } from 'react';
 import dynamic from 'next/dynamic';
 import _ from 'lodash';
-import { usePathname } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 // import { useConstructorDataAPI } from '@/app/actions/use-constructor';
 import { getDeviceType } from '@/lib/utils';
 import { useLayoutContext } from '@/context/LayoutContext';
 import LoadingPage from './loadingPage';
 import useSWR from 'swr';
+import { actionService, apiCallService, stateManagerService } from '@/services';
+import { TTypeSelectState, TVariable, TVariableMap } from '@/types';
+import { actionsStore, apiResourceStore, stateManagementStore } from '@/stores';
 
 const GridSystemContainer = dynamic(() => import('@/components/grid-systems'), {
   loading: () => <LoadingPage />,
@@ -23,31 +27,27 @@ const fetcher = async (url: string) => {
 
 export default function LayoutContent({ children }: { children: React.ReactNode }) {
   const { headerLayout, footerLayout, setHeaderLayout, setFooterLayout } = useLayoutContext();
-  const pathname = usePathname();
-
   const [deviceType, setDeviceType] = useState(getDeviceType());
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+  // Store hooks
+  const { addAndUpdateApiResource } = apiResourceStore();
+  const { setStateManagement } = stateManagementStore();
+  const { setActions } = actionsStore();
+
   const [stateLayoutIds, setStateLayoutIds] = useState({
     headerId: '',
     footerId: '',
   });
-
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-  const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
   const { data, error, isLoading } = useSWR(
     `${API_URL}/api/client/getLayout?pId=${projectId}&uid=${pathname}`,
     fetcher,
     { revalidateOnFocus: false, refreshInterval: 60000 }
   );
-
-  // const {
-  //   headerLayout: newHeaderLayout,
-  //   footerLayout: newFooterLayout,
-  //   isLoading,
-  // } = useConstructorDataAPI(
-  //   undefined, // Không cần documentId vì chỉ lấy header/footer
-  //   pathname
-  // );
 
   useEffect(() => {
     if (typeof window === 'undefined') return;
@@ -91,6 +91,72 @@ export default function LayoutContent({ children }: { children: React.ReactNode 
     () => (footerLayout && (footerLayout as Record<string, any>)[deviceType]) ?? footerLayout ?? {},
     [footerLayout, deviceType]
   );
+
+  const uid: any =
+    searchParams.get('uid') ||
+    (pathname === '/' ? process.env.NEXT_PUBLIC_DEFAULT_UID : pathname.slice(1));
+
+  const getActions = async () => {
+    try {
+      const result = await actionService.getData({ uid, projectId: projectId || '' });
+      if (!_.isEmpty(result?.data?.data)) setActions(result.data.data);
+    } catch (error) {
+      console.log('🚀 ~ getActions ~ error:', error);
+    }
+  };
+
+  const getStates = async () => {
+    const list: TTypeSelectState[] = [
+      'appState',
+      'componentState',
+      'globalState',
+      'apiResponse',
+      'dynamicGenerate',
+    ];
+    try {
+      await Promise.all(
+        list.map(async (type) => {
+          const result = await stateManagerService.getData(
+            type === 'globalState'
+              ? { projectId: projectId || '', type }
+              : { uid: uid || 'home', projectId: projectId || '', type }
+          );
+          if (_.isEmpty(result?.data)) return;
+          const { state } = result?.data;
+          if (_.isEmpty(state)) return;
+          setStateManagement({
+            type,
+            dataUpdate: state.reduce(
+              (acc: TVariableMap, item: TVariable) => ({
+                ...acc,
+                [item.id]: item,
+              }),
+              {}
+            ),
+          });
+        })
+      );
+    } catch (error) {
+      console.log('🚀 ~ getStates ~ error:', error);
+    }
+  };
+
+  const getApiCall = async () => {
+    try {
+      const result = await apiCallService.get({ uid, projectId: projectId || '' });
+      addAndUpdateApiResource({ apis: result?.data?.apis });
+    } catch (error) {
+      console.log('🚀 ~ getApiCall ~ error:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (!projectId) return;
+    getStates();
+    getApiCall();
+    getActions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [uid, projectId]);
 
   if (isLoading) return <LoadingPage />;
 
