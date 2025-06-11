2a94d6d7ac72acaaf634aeaafcc2fb307a4488e2
khoi_2212 <kkhoi600@gmail.com>
2025-06-11 11:39:06 +0700

    create useInitState

--- components/commons/Modal.tsx
+++ components/commons/Modal.tsx
@@ -1,41 +1,9 @@
-'use client';
+import React from 'react';
 
-import React, { useEffect } from 'react';
-import ReactModal from 'react-modal';
+type Props = object;
 
-const Modal = ({ isOpen, onClose, children, ...props }: any) => {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const appRoot = document.getElementById('__next');
-      if (appRoot) {
-        ReactModal.setAppElement(appRoot);
-      }
-    }
-  }, []);
-
-  return (
-    <ReactModal
-      isOpen={isOpen}
-      onRequestClose={onClose}
-      shouldCloseOnOverlayClick={true}
-      style={{
-        content: {
-          background: 'lightblue',
-          padding: '30px',
-          borderRadius: '10px',
-          maxWidth: '500px',
-          margin: 'auto',
-        },
-        overlay: {
-          backgroundColor: 'rgba(0, 0, 0, 0.7)',
-        },
-      }}
-      {...props}
-    >
-      {children}
-      <button onClick={onClose}>Đóng</button>
-    </ReactModal>
-  );
+const Page: React.FC<Props> = ({}) => {
+  return <div>Page</div>;
 };
 
-export default Modal;
+export default Page;
---
+++ services/authSetting/index.ts
@@ -0,0 +1,22 @@
+import axiosInstance from '@/configs/axiosInstance';
+import { TApiResponse } from '@/types';
+
+import { TAuthSetting, TAuthSettingUpdate } from './type';
+
+const create = async (data: TAuthSetting): Promise<TApiResponse<TAuthSetting>> => {
+  const res = await axiosInstance.post(`/authSetting`, data);
+  return res.data;
+};
+const update = async (data: TAuthSettingUpdate): Promise<TApiResponse<TAuthSetting>> => {
+  const res = await axiosInstance.put(`/authSetting`, data);
+  return res.data;
+};
+const get = async (data: { projectId?: string }): Promise<TApiResponse<TAuthSetting>> => {
+  const res = await axiosInstance.get(`/authSetting`, { params: data });
+  return res.data;
+};
+export const authSettingService = {
+  create,
+  update,
+  get,
+};
