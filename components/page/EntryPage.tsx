003401c14fafbec68ab86bc54d87c15711ffac19
khoi_2212 <kkhoi600@gmail.com>
2025-06-17 16:19:03 +0700

    update page main

--- components/page/EntryPage.tsx
+++ components/page/EntryPage.tsx
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+import { Metadata } from 'next';
 import Head from 'next/head';
 import { headers } from 'next/headers';
 import { FC, Suspense } from 'react';
@@ -9,7 +11,7 @@ import { RenderUIClient } from '../grid-systems/ClientWrapGridSystem';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
-interface Metadata {
+interface MetadataIcon {
   data?: {
     form?: {
       icon?: {
@@ -18,6 +20,83 @@ interface Metadata {
         shortcut?: string;
       };
     };
+  };
+}
+export async function generateMetadata(): Promise<Metadata> {
+  const path = 'NextJS';
+
+  const metadata = await fetchMetadata(path);
+  const formMetadata = _.get(metadata, 'data.form');
+
+  if (!formMetadata) {
+    return {
+      title: 'NextJS',
+      description: 'NextJS 15',
+    };
+  }
+  const iconConfig = {
+    icon: _.get(formMetadata, 'icon.icon'),
+    shortcut: _.get(formMetadata, 'icon.shortcut'),
+    apple: _.get(formMetadata, 'icon.apple'),
+  };
+
+  return {
+    title: {
+      default: formMetadata?.title?.default || 'NextJS PAGE',
+      template: formMetadata?.title.template,
+    },
+    description: formMetadata?.description || 'Default NextJS Page.',
+    keywords: formMetadata?.keywords,
+    authors: formMetadata?.authors?.map((author: any) => ({
+      name: author.name,
+      url: author.url,
+    })),
+    openGraph: {
+      title: formMetadata?.openGraph?.title || 'NEXTJS PAGE',
+      description: formMetadata?.openGraph?.description || 'Default NextJS page.',
+      url: formMetadata?.openGraph?.url,
+      siteName: formMetadata?.openGraph?.siteName,
+      images: formMetadata?.openGraph?.images?.map((image: any) => ({
+        url: image?.url,
+        width: image?.width,
+        height: image?.height,
+        alt: image?.alt,
+        secureUrl: image?.secure_url,
+        type: image?.type || 'image/jpeg',
+      })),
+      locale: formMetadata?.openGraph?.locale || 'en_US',
+      type: formMetadata?.openGraph?.type || 'website',
+      modifiedTime: formMetadata?.openGraph?.updated_time,
+    },
+    twitter: {
+      card: formMetadata?.twitter?.card || 'summary',
+      title: formMetadata?.twitter?.title,
+      description: formMetadata?.twitter?.description,
+      images: formMetadata?.twitter?.images,
+    },
+    robots: {
+      index: formMetadata?.robots?.index,
+      follow: formMetadata?.robots?.follow,
+      nocache: formMetadata?.robots?.nocache,
+      'max-snippet': formMetadata?.robots?.maxSnippet,
+      'max-video-preview': formMetadata?.robots?.maxVideoPreview,
+      'max-image-preview': formMetadata?.robots?.maxImagePreview,
+      googleBot: formMetadata?.robots?.googleBot
+        ? {
+            index: formMetadata?.robots?.googleBot?.index,
+            follow: formMetadata?.robots?.googleBot?.follow,
+            noimageindex: formMetadata?.robots?.googleBot?.noimageindex,
+          }
+        : undefined,
+    },
+    icons: {
+      icon: iconConfig.icon || undefined,
+      shortcut: iconConfig.shortcut || undefined,
+      apple: iconConfig.apple || undefined,
+    },
+    alternates: {
+      canonical: formMetadata?.alternates?.canonical || undefined,
+    },
   };
 }
 
@@ -41,7 +120,7 @@ const EntryPage: FC = async () => {
   const matchingPattern = getMatchingRoutePattern(pathname, patterns);
 
   // Fetch metadata
-  const metadata: Metadata = await fetchMetadata(matchingPattern || '');
+  const metadata: MetadataIcon = await fetchMetadata(matchingPattern || '');
 
   // Safely access metadata
   const formMetadata = metadata?.data?.form || {};
