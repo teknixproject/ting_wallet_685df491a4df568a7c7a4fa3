af18357743a8dd013f247afad491331e64bee1c2
khoi_2212 <kkhoi600@gmail.com>
2025-06-25 13:43:26 +0700

    update modal ui

--- components/commons/Modal.tsx
+++ components/commons/Modal.tsx
@@ -1,14 +1,11 @@
 'use client';
 
-import { useEffect } from 'react';
-import ReactModal from 'react-modal';
-
+import { Dialog, DialogContent, DialogOverlay } from '@/components/ui/dialog';
 import { useHandleData } from '@/hooks/useHandleData';
 import { useUpdateData } from '@/hooks/useUpdateData';
 import { GridItem } from '@/types/gridItem';
 
 import { RenderSlice } from '../grid-systems';
-import { StyleBox } from './StyleBox';
 
 interface ModalProps {
   data: any;
@@ -16,47 +13,24 @@ interface ModalProps {
 }
 
 const Modal = ({ data, ...props }: ModalProps) => {
-  console.log('🚀 ~ Modal ~ data:', data);
   const { dataState } = useHandleData({ dataProp: data?.data });
-  console.log('🚀 ~ Modal ~ dataState:', dataState);
 
   const { updateData } = useUpdateData({ dataProp: data?.data });
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const appRoot = document.getElementById('__next');
-      if (appRoot) {
-        ReactModal.setAppElement(appRoot);
-      }
-    }
-  }, []);
-
   return (
-    <StyleBox
-      as={ReactModal}
-      isOpen={dataState}
-      onRequestClose={updateData}
-      shouldCloseOnOverlayClick={true}
-      style={{
-        content: {
-          position: 'absolute',
-          background: 'lightblue',
-          padding: '30px',
-          borderRadius: '10px',
-          margin: 'auto',
-          zIndex: 1000,
-        },
-        overlay: {
-          backgroundColor: 'rgba(0, 0, 0, 0.7)',
-        },
-      }}
-      styledComponentCss={data?.styledComponentCss}
-      {...props}
-    >
-      {data?.childs?.map((item: GridItem) => (
-        <RenderSlice slice={item} key={item.id} />
-      ))}
-    </StyleBox>
+    <Dialog open={dataState} onOpenChange={updateData}>
+      <DialogOverlay />
+      <DialogContent
+        style={{
+          ...data.style,
+        }}
+        {...props}
+      >
+        {data?.childs?.map((item: GridItem) => (
+          <RenderSlice slice={item} key={item.id} />
+        ))}
+      </DialogContent>
+    </Dialog>
   );
 };
 
