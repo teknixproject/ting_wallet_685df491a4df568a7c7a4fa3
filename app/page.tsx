9bcb97bf1c0237acce51cd0ffb66c08c16b6c9dc
teknixproject <project@teknixcorp.com>
2025-07-11 23:40:20 +0700

    Delete file: app/page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import EntryPage from '@/components/page/EntryPage';
-
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
-export default async function Home() {
-  return <EntryPage />;
-}
